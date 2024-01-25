b60ee969180b24b32101d1283cc99aae7bdc8fe2
Kenil Kanani <143511057+Kenil-08@users.noreply.github.com>
2024-01-25 21:53:47 +0530

    Color-Switcher mini-project complete.

---
+++ Color-Switcher/app.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".box");
+
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',(e)=>{
+        if(e.target.id === "grey")
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id === "white")
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id === "blue")
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id === "yellow")
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id === "purple")
+            body.style.backgroundColor = e.target.id;
+        
+    })
+})
